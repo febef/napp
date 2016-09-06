6fbd072251716a690aec46b6db6240ce0d0ea124
febef <fexbef@gmail.com>
2016-09-05 22:48:37 -0300

    [!] add 'hola' component from folder.

---
+++ test/testcomp/hola/index.js
@@ -0,0 +1,18 @@
+
+
+
+
+var run = function(){
+  console.log("hola\n");
+};
+
+var stop = function () {
+  console.log("chauuuuu chicha!\n");
+};
+
+module.exports = {
+  run: run,
+  stop: stop,
+  dependencies: [],
+  subordinantes: []
+};
